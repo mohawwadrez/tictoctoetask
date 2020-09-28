27d0aa00730c7a37d68572360db18f364574477b
mohawwadrez <mohawwadrez@yahoo.com>
2020-09-28 20:55:38 +0330

    fix extra moves

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -18,11 +19,14 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
-      squares: squares , 
-      xIsNext:!this.state.xIsNext,
-    })
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -35,8 +39,15 @@ class Board extends React.Component {
   }
 
   render() {
-    // const status = "Next player: X";
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'winner : ' + winner;
+    
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
+   
     return (
       <div>
         <div className="status">{status}</div>
@@ -66,20 +77,36 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
       </div>
     );
   }
+}
+
+function calWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Game />,
+        <Game />
       </header>
     </div>
   );
